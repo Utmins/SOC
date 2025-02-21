c7481e6acf9bc4ce6261bb278af1afe71a741481
Du8ePrehT <120234987+Utmins@users.noreply.github.com>
2025-02-20 23:29:47 -0800

    Add files via upload

---
+++ Windows/Credentials Extraction/Scripts/GetAllDomainUserPermission.ps1
@@ -0,0 +1,24 @@
+#Get all permissions in the domain, filtered to the two critical replication permissions represented by their GUIDs
+Import-Module ActiveDirectory
+
+# Replace with distinguished name of your domain
+cd 'AD:DC=eagle,DC=local' 
+
+$AllReplACLs = (Get-AcL).Access | Where-Object {$_.ObjectType -eq '1131f6ad-9c07-11d1-f79f-00c04fc2dcd2' -or $_.ObjectType -eq '1131f6aa-9c07-11d1-f79f-00c04fc2dcd2'}
+
+#Filter this list to RIDs above 1000 which will exclude well-known Administrator groups
+foreach ($ACL in $AllReplACLs)
+{
+    $user = New-Object System.Security.Principal.NTAccount($ACL.IdentityReference)
+    $SID = $user.Translate([System.Security.Principal.SecurityIdentifier])
+    $RID = $SID.ToString().Split("-")[7]
+    if([int]$RID -gt 1000)
+    {
+        Write-Host "Permission to Sync AD granted to:" $ACL.IdentityReference
+    }
+}
+
+#Get back to local domain name
+cd 'C:'
+Remove-Module ActiveDirectory
+
