b6f8c4325ee882aaa5ca536966ae195bba1195a3
Du8ePrehT <120234987+Utmins@users.noreply.github.com>
2025-04-08 14:52:17 -0700

    Update file_and_section_hashing.py

--- DFIR/HTB/Scripts/file_and_section_hashing.py
+++ DFIR/HTB/Scripts/file_and_section_hashing.py
@@ -2,15 +2,15 @@ import sys
 import pefile
 import peutils
 
-  pe_file = sys.argv[1]
-	pe = pefile.PE(pe_file)
-	imphash = pe.get_imphash()
-	print ("ImpHash is\n",imphash)
+pe_file = sys.argv[1]
+pe = pefile.PE(pe_file)
+imphash = pe.get_imphash()
+print ("ImpHash is\n",imphash)
 
-	print ("\nSection HASHes:\nMD5")
-	for section in pe.sections:
-	  print (section.Name, "MD5 hash:", section.get_hash_md5())
+print ("\nSection HASHes:\nMD5")
+for section in pe.sections:
+  print (section.Name, "MD5 hash:", section.get_hash_md5())
 
-	print ("SHA256")
-	for section in pe.sections:
-	  print (section.Name, "SHA256 hash:", section.get_hash_sha256())
+print ("SHA256")
+for section in pe.sections:
+  print (section.Name, "SHA256 hash:", section.get_hash_sha256())
