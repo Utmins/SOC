66b4e1253ab30afb2478e4dc6c097087332cc031
Du8ePrehT <120234987+Utmins@users.noreply.github.com>
2025-09-08 16:53:47 -0700

    Update run_example.ps1

--- Fishing/LABs/Email/Scripts/scripts/run_example.ps1
+++ Fishing/LABs/Email/Scripts/scripts/run_example.ps1
@@ -1,4 +1,4 @@
 Set-StrictMode -Version Latest
 $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 Set-Location (Join-Path $PSScriptRoot "..")
-python .\eml_safe_hash_algo_pw.py .\examples\sample.eml --algo sha256,md5
+python .\eml_attch_analyzer.py .\examples\sample.eml --algo sha256,md5
